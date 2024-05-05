3924f075ce583bd639cf96852ea9906f8b757272
rodfer0x80 <rodrigolf080@gmail.com>
2024-05-05 15:09:58 +0100

    run scripts for cloud

--- install.sh
+++ install.sh
@@ -3,3 +3,8 @@
 python -m venv .venv &&\
   ./.venv/bin/pip install -r requirements.txt &&\
   ./.venv/bin/python -m playwright install
+  
+ollama serve & 2>1 >/dev/null
+ollama pull gemma:2b
+ollama pull nomic-embed-text
+pkill ollama
