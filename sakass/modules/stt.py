955cc07241bad55b2bc661439b17d57608b6d68c
rodfer0x80 <rodrigolf080@gmail.com>
2024-05-10 18:55:35 +0100

    better tts using mozzilatts

--- sakass/modules/stt.py
+++ sakass/modules/stt.py
@@ -7,3 +7,11 @@ class STT:
 
   def audio_to_text(self, audio_data: bytes) -> str:
     return self.model.transcribe(audio_data)["text"]
+
+if __name__ == '__main__':
+  from pocketsphinx import AudioFile
+  from sakass.capabilities.audio_input import AudioInput
+  audio_input = AudioInput()
+  audio_data = audio_input.capture_audio()
+  audio_file = audio_input.save_audio_to_file(audio_data)
+  for phrase in AudioFile(audio_file): print(phrase)
